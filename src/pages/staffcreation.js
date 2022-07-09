f3b1be3c11f787ff032a4be42a2593a9b0b6fcf1
wabloan <wabloan@wabloan.com>
2022-07-09 15:19:08 +0100

    ok

--- src/pages/staffcreation.js
+++ src/pages/staffcreation.js
@@ -43,7 +43,7 @@ export default function Changepassword({update}) {
   const [btn,setbtn] = useState('Create Staff');
   const [disablebtn,setdisablebtn] = useState(false);
   const [allroles,setallroles] = useState([])
-   
+   const previleges=[];
   
     const handleUsername = (e) => {
       setusername(e.target.value)
@@ -176,6 +176,32 @@ export default function Changepassword({update}) {
         getStaffRole();
     },[])
 
+    const handleResource = (e)=>{
+     
+      if(e.target.checked){
+        if(!previleges.includes(e.target.value)){
+          previleges.push(e.target.value)
+        }
+      }
+      else if(!e.target.checked){
+        console.log('Unchecked');
+        if(previleges.length >  0){
+            for( let i = 0; i< previleges.length; i += 1){ 
+              console.log(previleges[i])
+              if ( previleges[i] === e.target.value) { 
+          
+                previleges.splice(i, 1); 
+              }
+          
+          }
+        
+        }
+        
+      }
+      console.log("previledge",previleges)
+     
+    }
+
   const theme = useTheme();
   return (
     <Page title="Create Staff">
@@ -248,36 +274,168 @@ export default function Changepassword({update}) {
                                     <div>
                                     
                                       <Checkbox 
+                                        value="Transaction History"
+                                        onChange={handleResource}
                                         inputProps={{ 'aria-label': 'controlled' }}
-                                        color="success" /> <span>Transaction Log</span>
+                                        color="success" /> <span>Transaction History</span>
                                     
                                     </div>
+                                    <div>
+                                    
+                                      <Checkbox 
+                                        value="Transaction Count"
+                                        onChange={handleResource}
+                                        inputProps={{ 'aria-label': 'controlled' }}
+                                        color="success" /> <span>Total transaction</span>
+                                    
+                                    </div>
+                                    <div>
+                                    
+                                      <Checkbox 
+                                        value="Customer Information"
+                                        onChange={handleResource}
+                                        inputProps={{ 'aria-label': 'controlled' }}
+                                        color="success" /> <span>Customer Information</span>
+                                    
+                                    </div>
+                                    <div>
+                                    
+                                      <Checkbox 
+                                        value="Customer Balances"
+                                        onChange={handleResource}
+                                        inputProps={{ 'aria-label': 'controlled' }}
+                                        color="success" /> <span>Customer Transaction Balances</span>
+                                    
+                                    </div>
+                                    <div>
+                                    
+                                      <Checkbox 
+                                        value="Disable 2FA"
+                                        onChange={handleResource}
+                                        inputProps={{ 'aria-label': 'controlled' }}
+                                        color="success" /> <span>Disable 2FA Autheticator</span>
+                                    
+                                    </div>
+                                    <div>
+                                    
+                                      <Checkbox 
+                                        value="Account Suspension"
+                                        onChange={handleResource}
+                                        inputProps={{ 'aria-label': 'controlled' }}
+                                        color="success" /> <span>Account Suspension</span>
+                                    
+                                    </div>
+                                    <div>
+                                    
+                                      <Checkbox 
+                                        value="Account Blockage"
+                                        onChange={handleResource}
+                                        inputProps={{ 'aria-label': 'controlled' }}
+                                        color="success" /> <span>Account Block</span>
+                                    
+                                    </div>
+                                    <div>
+                                    
+                                      <Checkbox 
+                                        value="Blacklist"
+                                        onChange={handleResource}
+                                        inputProps={{ 'aria-label': 'controlled' }}
+                                        color="success" /> <span>BlackList</span>
+                                    
+                                    </div>
+                                   
                               </div>
                               <div className='cover-2'>
+                                  <div>
+                                    
+                                    <Checkbox 
+                                      value="Transaction Log"
+                                      onChange={handleResource}
+                                      inputProps={{ 'aria-label': 'controlled' }}
+                                      color="default" /> 
+                                    <span>Transaction Log</span>
+                                    
+                                </div>
                                 <div>
-                                  
-                                  <Checkbox 
-                                    inputProps={{ 'aria-label': 'controlled' }}
-                                    color="default" /> 
-                                  <span>Transaction Count</span>
-                                  
-                              </div>
+                                    
+                                    <Checkbox 
+                                      value="Trade Log"
+                                      onChange={handleResource}
+                                      inputProps={{ 'aria-label': 'controlled' }}
+                                      color="default" /> 
+                                    <span>Trade Log</span>
+                                    
+                                </div>
+                                <div>
+                                    
+                                    <Checkbox 
+                                      value="All Deposit"
+                                      onChange={handleResource}
+                                      inputProps={{ 'aria-label': 'controlled' }}
+                                      color="default" /> 
+                                    <span>Deposit Log</span>
+                                    
+                                </div>
+                                <div>
+                                    
+                                    <Checkbox 
+                                     value="All Withdrawal"
+                                     onChange={handleResource}
+                                      inputProps={{ 'aria-label': 'controlled' }}
+                                      color="default" /> 
+                                    <span>Withdrawal Log</span>
+                                    
+                                </div>
+                                <div>
+                                    
+                                    <Checkbox 
+                                     value="Initialise Rate"
+                                     onChange={handleResource}
+                                      inputProps={{ 'aria-label': 'controlled' }}
+                                      color="default" /> 
+                                    <span>Rate Input</span>
+                                    
+                                </div>
                               </div>
                               <div className='cover-3'>
                                   <div>
                                     
                                     <Checkbox 
                                       inputProps={{ 'aria-label': 'controlled' }}
+                                      value="Asset Undermanagement Crypto"
+                                      onChange={handleResource}
                                       color="default" /> 
-                                    <span>Transaction Count</span>
+                                    <span>Asset Under Management Crypto</span>
                                     
                                 </div>
                                 <div>
                                     
                                     <Checkbox 
                                       inputProps={{ 'aria-label': 'controlled' }}
+                                      onChange={handleResource}
+                                      value="Asset Undermanagement Fiat"
                                       color="default" /> 
-                                    <span>BlackList</span>
+                                     <span>Asset Under Management Fiat</span>
+                                    
+                                </div>
+                                <div>
+                                    
+                                    <Checkbox 
+                                      value="Manual Deposit"
+                                      onChange={handleResource}
+                                      inputProps={{ 'aria-label': 'controlled' }}
+                                      color="default" /> 
+                                     <span>Manual Deposit</span>
+                                    
+                                </div>
+                                <div>
+                                    
+                                    <Checkbox
+                                      value="Manual Withdrawal" 
+                                      onChange={handleResource}
+                                      inputProps={{ 'aria-label': 'controlled' }}
+                                      color="default" /> 
+                                     <span>Manual Withdrawal</span>
                                     
                                 </div>
                               </div>
